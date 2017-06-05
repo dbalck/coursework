40d32268cd46f8a17938691c4752d24addd93cf5
Daniel Balck <daniel.balck@truvenhealth.com>
2017-06-05 13:51:59 -0400

    blah

---
+++ scripts/week2_lab.sh
@@ -0,0 +1,3 @@
+#slcli vs create --hostname saltmaster --os CENTOS_LATEST_64 --datacenter wdc01 --domain example.com --billing hourly --cpu 1 --memory 1024 --key general
+slcli vs create --hostname dhost --os CENTOS_LATEST_64 --datacenter wdc01 --domain example.com --billing hourly --cpu 1 --memory 1024 --key general
+#slcli vs create --hostname logger --os CENTOS_LATEST_64 --datacenter wdc01 --domain example.com --billing hourly --cpu 1 --memory 2048 --key general
---
+++ scripts/week4_hw.sh
@@ -0,0 +1,4 @@
+slcli vs create --hostname host1 --domain example.com --datacenter wdc01 --os CENTOS_7 --cpu 2 --memory 2048 --wait 2000
+slcli vs create --hostname host2 --domain example.com --datacenter wdc01 --os CENTOS_7 --cpu 2 --memory 2048 --wait 2000
+slcli vs create --hostname host3 --domain example.com --datacenter wdc01 --os CENTOS_7 --cpu 2 --memory 2048 --wait 2000
+
