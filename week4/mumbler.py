d5d06662b793fcd0eb76106711fd511cc4413102
Daniel Balck <daniel.balck@berkeley.edu>
2017-06-07 01:51:12 -0400

    mumbling...:

--- week4/mumbler.py
+++ week4/mumbler.py
@@ -7,6 +7,7 @@ from contextlib import contextmanager
 import socket
 import string
 import csv
+import random
 
 url_template = "http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-{}.csv.zip"
 start_file_idx = 0
@@ -32,21 +33,33 @@ class Mumbler(object):
         for i in open(filename):
             if (i.startswith(word)): yield i
 
+    def pick_next(self, probs):
+        rand = random.random()
+        cumprob = 0
+        for key, value in probs.iteritems():
+            cumprob += value
+            if cumprob > rand: return key.split(" ")[1]
+
+
     def mumble(self, word):
         local_indices = []
         for ch in self.get_local_chunks():
             local_indices.append("/gpfs/gpfsfpo/index-%s" % ch.strip())
         print local_indices
-        data = [] 
+        incidence = 0
         for f in local_indices:
             with open(f) as csvfile:
                 data = csv.reader(csvfile, delimiter="\t", quoting=csv.QUOTE_NONE)
                 file_as_list = [i for i in data]
                 filtered = filter(lambda x: x[0].split(" ")[0] == word, file_as_list) 
-                print filtered
-                prior = reduce(lambda x, y: x[1] + y[1], filtered)
-                print prior
-
+                if not filtered: return None 
+                col = [row[1] for row in filtered]
+                incidence = reduce(lambda x, y: int(x) + int(y), col)
+                mapped = map(lambda x: {x[0]: int(x[1]) / float(incidence)}, filtered)
+                probs = {}
+                for d in mapped:
+                    probs.update(d)
+                return self.pick_next(probs)
 
 
 def main():
