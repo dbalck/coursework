c3090e2aebca78ebaede153732ffdd02dc40acce
u400945 <dbalck@localhost.localdomain>
2017-07-19 16:58:43 -0400

    trying to work out the pop user algorithm

--- week9/hw/src/main/java/PopHash.java
+++ week9/hw/src/main/java/PopHash.java
@@ -29,7 +29,8 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
 import scala.Tuple2;
-import twitter4j.Status;
+import scala.Tuple3;
+import twitter4j.*;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -69,116 +70,50 @@ public class PopHash {
     if (!sparkConf.contains("spark.master")) {
       sparkConf.setMaster("local[2]");
       System.out.println("running local mode, spark.master not found");
-        
     }
 
-    JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
+    JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(5000));
     JavaReceiverInputDStream<Status> stream = TwitterUtils.createStream(jssc, filters);
 
-    JavaDStream<String> words = stream.flatMap(new FlatMapFunction<Status, String>() {
-      @Override
-      public Iterator<String> call(Status s) {
-        return Arrays.asList(s.getText().split(" ")).iterator();
-      }
-    });
+    // map the tweet to its text as a k-v
+    JavaPairDStream<Status, String> statusToText = stream
+      .mapToPair(s -> new Tuple2<Status, String>(s, s.getText()));
 
-    JavaDStream<String> hashTags = words.filter(new Function<String, Boolean>() {
-      @Override
-      public Boolean call(String word) {
-        return word.startsWith("#");
-      }
-    });
+    // map the #hashtag words in the value text to the tweet
+    JavaPairDStream<Status, String> statusToWord = statusToText
+      .flatMapValues(text -> Arrays.asList(text.split(" ")).iterator());
 
-    // Read in the word-sentiment list and create a static RDD from it
-    String wordSentimentFilePath = "streaming-twitter/examples/data/AFINN-111.txt";
-    final JavaPairRDD<String, Double> wordSentiments = jssc.sparkContext()
-      .textFile(wordSentimentFilePath)
-      .mapToPair(new PairFunction<String, String, Double>(){
-        @Override
-        public Tuple2<String, Double> call(String line) {
-          String[] columns = line.split("\t");
-          return new Tuple2<>(columns[0], Double.parseDouble(columns[1]));
-        }
-      });
+    // filter for the #hashtag words in the value text to the tweet
+    JavaPairDStream<Status, String> statusToHashTag = statusToWord
+      .filter(pair -> pair._2().startsWith("#")); 
 
-    JavaPairDStream<String, Integer> hashTagCount = hashTags.mapToPair(
-      new PairFunction<String, String, Integer>() {
-        @Override
-        public Tuple2<String, Integer> call(String s) {
-          // leave out the # character
-          return new Tuple2<>(s.substring(1), 1);
-        }
-      });
+    // filter the @user words in the value text to the tweet
+    JavaPairDStream<Status, String> statusToUser = statusToWord
+      .filter(pair -> pair._2().startsWith("@")); 
 
-    JavaPairDStream<String, Integer> hashTagTotals = hashTagCount.reduceByKeyAndWindow(
-      new Function2<Integer, Integer, Integer>() {
-        @Override
-        public Integer call(Integer a, Integer b) {
-          return a + b;
-        }
-      }, new Duration(10000));
+    // count the frequencies of the hashtags
+    JavaPairDStream<String, Long> hashTagToCount = statusToHashTag
+      .countByValueAndWindow(new Duration(10000));
 
-    // Determine the hash tags with the highest sentiment values by joining the streaming RDD
-    // with the static RDD inside the transform() method and then multiplying
-    // the frequency of the hash tag by its sentiment value
-    JavaPairDStream<String, Tuple2<Double, Integer>> joinedTuples =
-      hashTagTotals.transformToPair(new Function<JavaPairRDD<String, Integer>,
-        JavaPairRDD<String, Tuple2<Double, Integer>>>() {
-        @Override
-        public JavaPairRDD<String, Tuple2<Double, Integer>> call(
-            JavaPairRDD<String, Integer> topicCount) {
-          return wordSentiments.join(topicCount);
-        }
-      });
+    // sort the hashtags from most frequent to least
+    JavaPairDStream<Long, String> countToHashTagSorted = hashTagToCount
+      .mapToPair(x -> x.swap());
+      .transformToPair(rdd -> rdd.sortByKey(false));
 
-    JavaPairDStream<String, Double> topicHappiness = joinedTuples.mapToPair(
-      new PairFunction<Tuple2<String, Tuple2<Double, Integer>>, String, Double>() {
-        @Override
-        public Tuple2<String, Double> call(Tuple2<String,
-          Tuple2<Double, Integer>> topicAndTuplePair) {
-          Tuple2<Double, Integer> happinessAndCount = topicAndTuplePair._2();
-          return new Tuple2<>(topicAndTuplePair._1(),
-            happinessAndCount._1() * happinessAndCount._2());
-        }
-      });
+    // swap the status message with the hashtags
+    JavaPairDStream<String, Status> hashTagToStatus = statusToHashTag
+      .mapToPair(x -> x.swap());
 
-    JavaPairDStream<Double, String> happinessTopicPairs = topicHappiness.mapToPair(
-      new PairFunction<Tuple2<String, Double>, Double, String>() {
-        @Override
-        public Tuple2<Double, String> call(Tuple2<String, Double> topicHappiness) {
-          return new Tuple2<>(topicHappiness._2(),
-            topicHappiness._1());
-        }
-      });
+    JavaPairDStream<String, Integer> hashTagMentions = statusToHashTags
+      .mapToPair(hash -> new Tuple2<String, Integer>(hash, 1))
+      .reduceByKeyAndWindow((x,y) -> x + y, new Duration(5000));
 
-    JavaPairDStream<Double, String> happiest10 = happinessTopicPairs.transformToPair(
-      new Function<JavaPairRDD<Double, String>, JavaPairRDD<Double, String>>() {
-        @Override
-        public JavaPairRDD<Double, String> call(
-            JavaPairRDD<Double, String> happinessAndTopics) {
-          return happinessAndTopics.sortByKey(false);
-        }
-      }
-    );
 
-    // Print hash tags with the most positive sentiment values
-    happiest10.foreachRDD(new VoidFunction<JavaPairRDD<Double, String>>() {
-      @Override
-      public void call(JavaPairRDD<Double, String> happinessTopicPairs) {
-        List<Tuple2<Double, String>> topList = happinessTopicPairs.take(10);
-        System.out.println(
-          String.format("\nHappiest topics in last 10 seconds (%s total):",
-            happinessTopicPairs.count()));
-        for (Tuple2<Double, String> pair : topList) {
-          System.out.println(
-            String.format("%s (%s happiness)", pair._2(), pair._1()));
-        }
-      }
-    });
+    countToHashTagprint();
 
     jssc.start();
     try {
-      jssc.awaitTermination();
+      jssc.awaitTerminationOrTimeout(11000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
